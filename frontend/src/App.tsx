d18ded79a0c3db208deaed88404a74a2653f20b4
Gabriel Rodrigues Leite <88800090+gabrielrodriguesleite@users.noreply.github.com>
2025-04-09 01:12:32 -0300

    fix - cleanup old context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,23 @@
-import { useState } from "react"
 import { Routes, Route } from "react-router"
-import { MyGlobalContext } from "./Context"
 import Home from "./routes/Home"
 import Login from "./routes/Login"
 import Register from "./routes/Register"
 import { NotFound } from "./routes/NotFound"
 import { AuthProvider } from "./contexts/AuthProvider"
-import './App.css'
 import { NewsProvider } from "./contexts/NewsProvider"
+import './App.css'
 
 export function App() {
-  const [state, setState] = useState({ token: "", user: { name: "", email: "" } })
   return (
-    <MyGlobalContext.Provider value={{ ...state, setState }} >
-      <AuthProvider>
-        <NewsProvider>
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
-            <Route path="register" element={<Register />} />
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </NewsProvider>
-      </AuthProvider>
-    </MyGlobalContext.Provider>
+    <AuthProvider>
+      <NewsProvider>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </NewsProvider>
+    </AuthProvider>
   )
 }
